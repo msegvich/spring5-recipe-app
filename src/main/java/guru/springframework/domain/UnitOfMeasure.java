8fd3f3c2c0d8ca7cba2b6fe020ec839a047a28c4
Matt Segvich <matt.segvich@slalom.com>
2017-10-27 14:35:54 -0500

    Fixed issue with init of DB

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -11,7 +11,7 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String uom_label;
+    private String description;
 
     public Long getId() {
         return id;
@@ -21,11 +21,11 @@ public class UnitOfMeasure {
         this.id = id;
     }
 
-    public String getUom() {
-        return uom_label;
+    public String getDescription() {
+        return description;
     }
 
-    public void setUom(String uom) {
-        this.uom_label = uom;
+    public void setDescription(String uom) {
+        this.description = uom;
     }
 }
